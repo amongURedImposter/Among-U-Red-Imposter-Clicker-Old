f0f4cc597d0ca3a86681b733195801f835239809
Jonathon50 <93009498+Jonathon50@users.noreply.github.com>
2022-02-25 11:43:02 -0600

    Update main.js

--- main.js
+++ main.js
@@ -13488,7 +13488,7 @@ Game.Launch=function()
 		if (!Game.OnAscend)
 		{
 			
-			var unit=(Math.round(Game.cookiesd)==1?' cookie':' crewmates killed');
+			var unit=(Math.round(Game.cookiesd)==1?' crewmate killed':' crewmates killed');
 			var str=Beautify(Math.round(Game.cookiesd));
 			if (Game.cookiesd>=1000000)//dirty padding
 			{
